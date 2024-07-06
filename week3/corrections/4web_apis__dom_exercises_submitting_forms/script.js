04c128d487aa507237e92417b17cd5f1be0a593b
Mahmoud Khudairi <mahmoudkhudairi@gmail.com>
2024-07-06 18:15:42 +0300

    dom2

--- week3/corrections/4web_apis__dom_exercises_submitting_forms/script.js
+++ week3/corrections/4web_apis__dom_exercises_submitting_forms/script.js
@@ -1,1 +1,48 @@
 // You can work here or download the template
+
+/*
+Add an event listener to handle form submission.
+Validate that all fields are not empty.
+If validation passes, output the form data to the console and display it in the p element as a list (ul)
+If not output an error message in the p element, style it as an error. Maybe something red and flashy?
+Make sure to toggle the error and success styles! 
+Clear the form fields
+
+*/
+const form = document.querySelector('#contact-form');
+
+const outputParagraph = document.querySelector('#output');
+const ul = document.createElement('ul');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  // get values
+  if (!e.target.name.value || !e.target.email.value || !e.target.message.value) {
+    outputParagraph.textContent = 'Please fill in all the fields.';
+    outputParagraph.classList.remove('text-green-500');
+    outputParagraph.classList.add('text-red-500');
+  } else {
+    // cleanup
+    // outputParagraph.innerHTML = '';
+    // ul.innerHTML = '';
+    // for (let index = 0; index < e.target.elements.length; index++) {
+    //   const element = e.target.elements[index];
+    //   console.log(element.id, element.value);
+    //   const li = document.createElement('li');
+    //   if (element.id) {
+    //     li.textContent = `${element.id}: ${element.value}`;
+    //     ul.appendChild(li);
+    //   }
+    // }
+    // outputParagraph.appendChild(ul);
+    const template = `<ul>
+    <li> name: ${e.target.name.value}</li>
+    <li> email: ${e.target.email.value}</li>
+    <li> message: ${e.target.message.value}</li>
+    </ul>`;
+    outputParagraph.innerHTML = template;
+    outputParagraph.classList.remove('text-red-500');
+    outputParagraph.classList.add('text-green-500');
+    e.target.reset();
+  }
+  // console.log(e.target.name.value);
+});
--- week3/corrections/5web_apis__dom_exercises_rock,_paper,_scissors/script.js
+++ week3/corrections/5web_apis__dom_exercises_rock,_paper,_scissors/script.js
@@ -1,1 +1,68 @@
 // You can work here or download the template
+
+/**
+ * 
+ HTML with Tailwind CSS:
+
+A card at the top to display the score (User vs. Computer).
+A card with three buttons for selecting Rock, Paper, or Scissors.
+An empty card to display the result of each round.
+A button to play the round.
+JavaScript:
+
+Listen for a click on the play button.
+Check if the user selected something; if not, alert the user.
+Mark the user selected button in some way.
+Randomly select Rock, Paper, or Scissors for the computer.
+Compare the user's selection with the computer's selection.
+Display the result in the output area.
+Update the score and the DOM accordingly.
+ */
+const userScoreEl = document.querySelector('#user-score');
+const computerScoreEl = document.querySelector('#computer-score');
+const selectionButtons = document.querySelectorAll('#selection button');
+const result = document.querySelector('#result');
+const playButton = document.querySelector('#play-button');
+let userScore = 0;
+let computerScore = 0;
+const choices = ['paper', 'rock', 'scissors'];
+let userChoice = '';
+selectionButtons.forEach(btn => btn.addEventListener('click', e => selectChoice(e.target.id)));
+function selectChoice(choice) {
+  userChoice = choice;
+  selectionButtons.forEach(btn => {
+    if (btn.id === choice) {
+      btn.classList.add('line-through');
+    } else {
+      btn.classList.remove('line-through');
+    }
+  });
+}
+
+playButton.addEventListener('click', playRound);
+function playRound(e) {
+  if (!userChoice) {
+    alert('Please select a choice!');
+    return;
+  }
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  const resultStr = `you picked ${userChoice} & computer picked ${computerChoice}`;
+  if (userChoice === computerChoice) {
+    // tie
+    result.textContent = `${resultStr} it's a tie!`;
+  } else if (
+    (userChoice === 'rock' && computerChoice === 'scissors') ||
+    (userChoice === 'paper' && computerChoice === 'rock') ||
+    (userChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    // user won!
+    result.textContent = `${resultStr} You won!`;
+    userScore++;
+  } else {
+    // computer won
+    result.textContent = `${resultStr} computer won!`;
+    computerScore++;
+  }
+  userScoreEl.textContent = userScore;
+  computerScoreEl.textContent = computerScore;
+}
